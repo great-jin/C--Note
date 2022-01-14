5bf92d6358196d882c80044ad90f6905834e0f5f
yweijin <yweijin@linewell.com>
2022-01-14 17:42:54 +0800

    java idea

---
+++ Java/src/code/CollectionTest.java
@@ -0,0 +1,7 @@
+package code;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java/src/code/User.java
@@ -0,0 +1,11 @@
+package code;
+
+public class User {
+    private int ID;
+    private String userName;
+
+    public User(int ID, String userName){
+        this.ID = ID;
+        this.userName = userName;
+    }
+}
